550ecc8b44f55dc92b68c92a39b5601e9fc8b154
x0reg <benauto2197@gmail.com>
2025-03-11 22:01:18 +0700

    đ

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Simple HTML Page</title>
 </head>
+
 <body>
     <h1>Hello, World fixer admin to larravel app!</h1>
     <p>tesst auto deploy github action.</p>
 </body>
+
 </html>
