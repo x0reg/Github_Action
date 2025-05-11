da410c4842e1a2e79736a066a19e119c722eb827
x0reg <benauto2197@gmail.com>
2025-05-11 22:13:40 +0700

    test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Simple HTML Page</title>
+    <title>My Simple HTML Page ok bạn</title>
 </head>
 
 <body>
